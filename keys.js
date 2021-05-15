f1b4c92a0eddd036348ce03b2f4b4d6266bca3b4
fedekolo <fedekolonski@gmail.com>
2021-05-15 20:47:52 -0300

    prueba heroku

--- keys.js
+++ keys.js
@@ -1,21 +1,21 @@
 module.exports = {
 
     // HEROKU
-    // database: {
-    //         host: 'us-cdbr-east-03.cleardb.com',
-    //         user: 'bd3a259f4a4ff9',
-    //         password:'a92d0042',
-    //         database: 'heroku_5e04ceac3204daf',
-    //         port:3306
-    // }
+    database: {
+            host: 'us-cdbr-east-03.cleardb.com',
+            user: 'b24eab56dd5dee',
+            password:'baa9ffde',
+            database: 'heroku_06a0e15d0f50598',
+            port:3306
+    }
 
     // LOCALBASE
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password:'',
-        database: 'db_moscovicz',
-        port:3306
-}
+//     database: {
+//         host: 'localhost',
+//         user: 'root',
+//         password:'',
+//         database: 'db_moscovicz',
+//         port:3306
+// }
 
 }
