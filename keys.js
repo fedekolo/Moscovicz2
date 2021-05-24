4a4c6b9a92d00f73dd1c9e9ea4e66c560de74416
fedekolo <fedekolonski@gmail.com>
2021-05-24 14:41:55 -0300

    Inicio Heroku

--- keys.js
+++ keys.js
@@ -2,20 +2,18 @@ module.exports = {
 
     // HEROKU
     // database: {
-    //         host: 'us-cdbr-east-03.cleardb.com',
-    //         user: 'b24eab56dd5dee',
-    //         password:'baa9ffde',
-    //         database: 'heroku_06a0e15d0f50598',
-    //         port:3306
-    // }
+            host: 'us-cdbr-east-03.cleardb.com',
+            user: 'b24eab56dd5dee',
+            password:'baa9ffde',
+            database: 'heroku_06a0e15d0f50598',
+            port:3306
+    }
 
     // LOCALBASE
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password:'',
-        database: 'db_moscovicz',
-        port:3306
-}
-
-}
+//     database: {
+//         host: 'localhost',
+//         user: 'root',
+//         password:'',
+//         database: 'db_moscovicz',
+//         port:3306
+// }
